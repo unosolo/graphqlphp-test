ff804ae855255d18ffe81c28c9ad84320e3196f5
unosolo <jchbarreto@gmail.com>
2022-11-05 13:53:39 -0300

    trim unnecessary code from  GqlSimpleObjectType service

--- src/GqlSimpleObjectType.php
+++ src/GqlSimpleObjectType.php
@@ -2,29 +2,14 @@
 
 namespace GqlSimple;
 
-use GraphQL\Type\Definition\ObjectType;
-use GraphQL\Type\Definition\Type;
-
 class GqlSimpleObjectType {
-	public function queryType(): ObjectType {
-		return new ObjectType([
-			'name' => 'Query',
-			'fields' => [
-				'echo' => [
-					'type' => Type::string(),
-					'resolve' => function( $rootValue, array $args): string {
-						error_log(var_export($rootValue), 3, 'debug.log');
-						error_log(var_export($args), 3, 'debug.log');
-						return 'Hello world';
-					}
-				]
-			]
-		]);
-	}
 
 	public function all(): array {
+		$echos = 'GqlSimple\Types\Echos';
+
 		return [
-			'query' => $this->queryType()
+			'query' => (new $echos())->get()
 		];
 	}
+
 }
